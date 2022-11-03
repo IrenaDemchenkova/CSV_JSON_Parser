7ffeedba3dc4b964b28f43facae387dc5f704b87
IrenaDemchenkova <iren.demchenkova@gmail.com>
2022-11-03 14:08:42 +0300

    Initial commit

---
+++ src/main/java/ru/IrenaDemchenkova/Main.java
@@ -0,0 +1,58 @@
+package ru.IrenaDemchenkova;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.opencsv.CSVReader;
+import com.opencsv.bean.ColumnPositionMappingStrategy;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+public class Main {
+    public static List<Employee> parseCSV(String[] columnMapping, String fileName) {
+        try (CSVReader reader = new CSVReader(new FileReader(fileName))) {
+            ColumnPositionMappingStrategy<Employee> strategy = new ColumnPositionMappingStrategy<>();
+            strategy.setType(Employee.class);
+            strategy.setColumnMapping(columnMapping);
+            CsvToBean<Employee> csv = new CsvToBeanBuilder<Employee>(reader)
+                    .withMappingStrategy(strategy)
+                    .build();
+            return csv.parse();
+        } catch (IOException e) {
+            System.out.println(e.getLocalizedMessage());
+        }
+        return null;
+    }
+
+    public static String listToJson(List<Employee> list) {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        return gson.toJson(list);
+    }
+
+    public static void writeString(String json, String fileName) {
+        try (FileWriter writer = new FileWriter(fileName)) {
+            writer.write(json);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
+        String fileName = "data.csv";
+        List<Employee> list = parseCSV(columnMapping, fileName);
+        if (list == null) {
+            System.out.println("Staff is null!");
+            return;
+        }
+        for (Employee employee : list) {
+            System.out.println(employee);
+        }
+        String json = listToJson(list);
+        writeString(json, "result.json");
+    }
+}
